4b0e84a37d81d3b3e11604bd274750d2037a07b0
James <lo.jimmy1994@gmail.com>
2018-03-25 20:55:22 +0800

    Added user model
    Done testing with user model
    
    Done testing with user model

--- src/server/models/user.ts
+++ src/server/models/user.ts
@@ -1,19 +1,36 @@
 import * as Sequelize from 'Sequelize';
-import { sequelize } from 'database';
+import { sequelize } from '../database';
 
-const User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING,
-  },
-  lastName: {
-    type: Sequelize.STRING,
+const User = sequelize.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    unique: true,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
   },
   username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  displayName: {
     type: Sequelize.STRING,
   },
   password: {
     type: Sequelize.STRING,
   },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    validate: {
+      isEmail: {
+        msg: 'Invalid email.',
+      },
+    },
+  },
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE,
 });
 
 export default User;
