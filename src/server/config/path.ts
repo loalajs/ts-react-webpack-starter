723d3deaf4d9d73fce2eb99d9bffda199223ef9e
James <lo.jimmy1994@gmail.com>
2018-04-23 23:57:15 +0800

    Fixed the client and server dist static path

--- src/server/config/path.ts
+++ src/server/config/path.ts
@@ -2,8 +2,8 @@ import * as path from 'path';
 const root = process.cwd();
 
 const appPaths = {
-  CLIENT_ROOT_PATH: path.join(root, 'client', 'app'),
-  SERVER_STATIC_PATH: path.join(root, 'server', 'statics'),
+  CLIENT_ROOT_PATH: path.join(root, 'dist' , 'client', 'app'),
+  SERVER_STATIC_PATH: path.join(root, 'dist', 'server', 'statics'),
 };
 
 export default appPaths;
